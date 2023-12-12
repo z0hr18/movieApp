7a93ba0074ed47d076e18cf5eb7fdf2574ed6afe
Zohra <guliyevaazohra@gmail.com>
2023-12-12 16:09:15 +0400

    commit

---
+++ MovieApp By Z/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp By Z
+//
+//  Created by Zohra Guliyeva on 12/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
